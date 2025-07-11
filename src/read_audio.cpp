6733ce8106ad156e806262ea8b54ee9c8ac34f15
Harry Newman <h.newman2005@outlook.com>
2025-07-11 14:20:07 +0100

    Changed names for readability sake

--- src/read_audio.cpp
+++ src/read_audio.cpp
@@ -3,26 +3,28 @@
 #include <cstring>
 #include <fstream>
 
-constexpr std::array<char, 4> riff{'R', 'I', 'F', 'F'};
+constexpr std::array<char, 4> expectedRiff{'R', 'I', 'F', 'F'};
+constexpr size_t wavHeaderSize = 44;
 
 void read_audio(const std::string& filename) {
 
-    // open file in binary mode
     std::ifstream wavFile(filename, std::ios::binary);
     if (!wavFile) {
         throw std::ios_base::failure("Failed to open file: " + filename);
     }
 
-    std::array<std::byte, 4> riffArray{};
+    // Read first 4 bytes and verify RIFF header
+    std::array<std::byte, 4> fileHeaderRiff{};
+    std::array<char, fileHeaderRiff.size()> temp_buffer{};
+    wavFile.read(temp_buffer.data(), fileHeaderRiff.size());
+    std::memcpy(fileHeaderRiff.data(), temp_buffer.data(), fileHeaderRiff.size());
 
-    std::array<char, riffArray.size()> temp_buffer{};
-    wavFile.read(temp_buffer.data(), riffArray.size());
-    std::memcpy(riffArray.data(), temp_buffer.data(), riffArray.size());
 
-
-    for (size_t i = 0; i < riffArray.size(); ++i) {
-        if (static_cast<char>(riffArray[i]) != riff[i]) {
+    for (size_t i = 0; i < fileHeaderRiff.size(); ++i) {
+        if (static_cast<char>(fileHeaderRiff[i]) != expectedRiff[i]) {
             throw std::runtime_error("Input file is not a valid WAV file.");
         }
     }
+
+    std::array<char, wavHeaderSize> wavHeader{};
 }
