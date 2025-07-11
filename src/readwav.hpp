c1a4a2d0c880b8c70cf1094a3086067b413d80bf
Harry Newman <h.newman2005@outlook.com>
2025-07-11 18:26:07 +0100

    [update] moved read_wav into a header file for use in main

---
+++ src/readwav.hpp
@@ -0,0 +1,42 @@
+#ifndef readwav_hpp
+#define readwav_hpp
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <string>
+#include <vector>
+
+constexpr size_t bits16perSample = 16;
+constexpr size_t nums = 30;
+
+// Struct for WavHeader
+#pragma pack(push, 1)
+struct WavHeader {
+  std::array<char, 4> riff;  // "RIFF"
+  uint32_t chunkSize;        // overall file size - 8 bytes
+  std::array<char, 4> wave;  // "WAVE"
+  std::array<char, 4> fmt;   // "fmt "
+  uint32_t subchunk1Size;    // 16 for PCM
+  uint16_t audioFormat;      // 1 = PCM, 3 = float
+  uint16_t numChannels;      // 1 = mono, 2 = stereo
+  uint32_t sampleRate;       // e.g. 44100 Hz
+  uint32_t byteRate;         // == SampleRate * NumChannels * BitsPerSample/8
+  uint16_t blockAlign;       // == NumChannels * BitsPerSample/8
+  uint16_t bitsPerSample;    // e.g. 16
+  std::array<char, 4> data;  // "data"
+  uint32_t dataSize;         // size of the sound data in bytes
+};
+#pragma pack(pop)
+
+struct WavFile {
+  WavHeader header;
+  std::vector<std::byte> soundData;
+};
+
+WavFile read_wav(const std::string& filename);
+std::vector<int16_t> convertBytes(WavHeader header,
+                                  std::vector<std::byte>& soundData);
+
+#endif
