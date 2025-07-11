4a7d42a7fdf2f8364a551431fe7f62ab1f7c610b
Harry Newman <h.newman2005@outlook.com>
2025-07-11 18:42:08 +0100

    Added try catch and args to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
+#include <exception>
 #include <iostream>
 
 #include "readwav.hpp"
 
-int main() {
-  WavFile wav = read_wav("../sound/file_example_WAV_10MG.wav");
+constexpr size_t nums = 30;
 
-  std::vector<int16_t> samples = convertBytes(wav.header, wav.soundData);
-
-  for (size_t i = 0; i < nums; ++i) {
-    std::cout << "Sample[" << i << "]: " << samples[i] << '\n';
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    std::cerr << "Usage: " << argv[0] << " <wav_file_path>\n";
+    return 1;
+  }
+  try {
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    WavFile wav = read_wav(argv[1]);
+    std::vector<int16_t> samples = convertBytes(wav.header, wav.soundData);
+    for (size_t i = 0; i < nums; ++i) {
+      std::cout << "Sample[" << i << "]: " << samples[i] << '\n';
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << '\n';
+    return 1;
   }
   return 0;
 }
