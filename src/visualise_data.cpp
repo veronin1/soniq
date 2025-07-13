f8253755d6f8599ff042f0a302a81d3b3449fcfa
Harry Newman <h.newman2005@outlook.com>
2025-07-13 19:37:46 +0100

    Range based for loop instead of iterative thanks to the lovely world of vectors

--- src/visualise_data.cpp
+++ src/visualise_data.cpp
@@ -15,8 +15,9 @@ void printBars(const std::vector<double>& magnitudes) {
 
   std::vector<double> scaledValues;
   double scaled = 0.0;
-  for (size_t i = 0; i < magnitudes.size(); ++i) {
-    scaled = (magnitudes[i] / largestMagnitude) * maxHeight;
+
+  for (double mag : magnitudes) {
+    scaled = (mag / largestMagnitude) * maxHeight;
     scaledValues.push_back(scaled);
   }
 }
