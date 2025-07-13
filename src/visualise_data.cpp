da17fb0d68413fb50d2f515a5e551c4c9c398889
Harry Newman <h.newman2005@outlook.com>
2025-07-13 19:37:11 +0100

    Scaling the values down

--- src/visualise_data.cpp
+++ src/visualise_data.cpp
@@ -1,12 +1,22 @@
 #include "visualise_data.hpp"
 
-#include <cstddef>
+#include <algorithm>
+#include <iostream>
 #include <vector>
+
+const double maxHeight = 40;
 
 void printBars(const std::vector<double>& magnitudes) {
   double largestMagnitude = 0;
 
   for (double integer : magnitudes) {
     largestMagnitude = std::max(largestMagnitude, integer);
+  }
+
+  std::vector<double> scaledValues;
+  double scaled = 0.0;
+  for (size_t i = 0; i < magnitudes.size(); ++i) {
+    scaled = (magnitudes[i] / largestMagnitude) * maxHeight;
+    scaledValues.push_back(scaled);
   }
 }
