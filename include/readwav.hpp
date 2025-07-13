51fc15b64fee89c9e01c17fea5e00ef283e25b92
Harry Newman <h.newman2005@outlook.com>
2025-07-13 18:54:46 +0100

    Added clarity to the comments on the wavHeader and made, used const instead of constexpr (I believe this is a slight optimisation), went from readwav_hpp -> READWAV_HPP

--- include/readwav.hpp
+++ include/readwav.hpp
@@ -1,31 +1,30 @@
-#ifndef readwav_hpp
-#define readwav_hpp
+#ifndef READWAV_HPP
+#define READWAV_HPP
 
 #include <array>
 #include <cstddef>
 #include <cstdint>
-#include <cstring>
 #include <string>
 #include <vector>
 
-constexpr size_t bits16perSample = 16;
+const int bits16perSample = 16;
 
-// Struct for WavHeader
 #pragma pack(push, 1)
 struct WavHeader {
   std::array<char, 4> riff;  // "RIFF"
-  uint32_t chunkSize;        // overall file size - 8 bytes
+  uint32_t chunkSize;        // Size of the entire file in bytes
   std::array<char, 4> wave;  // "WAVE"
   std::array<char, 4> fmt;   // "fmt "
-  uint32_t subchunk1Size;    // 16 for PCM
-  uint16_t audioFormat;      // 1 = PCM, 3 = float
-  uint16_t numChannels;      // 1 = mono, 2 = stereo
-  uint32_t sampleRate;       // e.g. 44100 Hz
-  uint32_t byteRate;         // == SampleRate * NumChannels * BitsPerSample/8
-  uint16_t blockAlign;       // == NumChannels * BitsPerSample/8
-  uint16_t bitsPerSample;    // e.g. 16
+  uint32_t subchunk1Size;    // Size of the fmt chunk (16 for PCM)
+  uint16_t audioFormat;      // Audio format (1 for PCM)
+  uint16_t numChannels;      // Number of channels (1 for mono, 2 for stereo)
+  uint32_t sampleRate;       // Sample rate (e.g., 44100 Hz)
+  uint32_t
+      byteRate;  // Byte rate (sampleRate * numChannels * bitsPerSample / 8)
+  uint16_t blockAlign;       // Block align (numChannels * bitsPerSample / 8)
+  uint16_t bitsPerSample;    // Bits per sample (e.g., 16)
   std::array<char, 4> data;  // "data"
-  uint32_t dataSize;         // size of the sound data in bytes
+  uint32_t dataSize;         // Size of the data chunk
 };
 #pragma pack(pop)
 
